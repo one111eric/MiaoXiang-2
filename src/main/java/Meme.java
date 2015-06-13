6a9fbb5ff7c688114654316534f0b0428595abaa
MiaoXiang <one111eric@hotmail.com>
2015-06-12 22:51:34 -0700

    2nd push

--- src/main/java/Meme.java
+++ src/main/java/Meme.java
@@ -11,86 +11,83 @@ import java.util.TreeMap;
 
 import org.json.*;
 
-
 public class Meme {
-    public Meme()
-    {
-    	Meme=new HashMap<String,Integer>();
-    }
-	public void ReadMemes() throws JSONException
-	{   
-		String content="";
+	//Constructor
+	public Meme() {
+		this.Meme = new HashMap<String, Integer>();
+	}
+    //Read Memes from JSON File
+	public void ReadMemes() throws JSONException {
+		String content = "";
 		try {
-			content = new Scanner(new File("src\\main\\resources\\memes.json")).useDelimiter("\\Z").next();
+			content = new Scanner(new File("src\\main\\resources\\memes.json"))
+					.useDelimiter("\\Z").next();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		JSONObject obj=new JSONObject(content);
-		JSONArray arr=obj.getJSONArray("memes");
-		for(int i=0;i<arr.length();i++)
-			{
-			String memestring=arr.getJSONObject(i).getString("memeString");
-			Meme.put(memestring,0);
-			}
+		JSONObject obj = new JSONObject(content);
+		JSONArray arr = obj.getJSONArray("memes");
+		for (int i = 0; i < arr.length(); i++) {
+			String memestring = arr.getJSONObject(i).getString("memeString");
+			this.Meme.put(memestring, 0);
+		}
 	}
-	public void SortMemes()
-	{
-		SortedMeme=new TreeMap<String,Integer>(Meme);
+   //Sort Memes by using TreeMap
+	public void SortMemes() {
+		SortedMeme = new TreeMap<String, Integer>(this.Meme);
 	}
-	public void ScoreMemes()
-	{
-		Random random=new Random();
-		
-		for(Map.Entry<String, Integer> entry:SortedMeme.entrySet())
-		{
-			int i=random.nextInt(10)+1;
+   //Score Memes with random number
+	public void ScoreMemes() {
+		Random random = new Random();
+
+		for (Map.Entry<String, Integer> entry : SortedMeme.entrySet()) {
+			int i = random.nextInt(10) + 1;
 			entry.setValue(i);
 		}
 	}
-	public void PrintMemes()
-	{
-		for(Map.Entry<String, Integer> entry:SortedMeme.entrySet())
-		{
-			System.out.println(entry.getKey()+": "+entry.getValue());
+    //print all memes for simple testing
+	public void PrintMemes() {
+		for (Map.Entry<String, Integer> entry : SortedMeme.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
 	}
-	public void WriteMemes()
-	{
-		JSONObject obj=new JSONObject();
-		
-		JSONArray list=new JSONArray();
-		
-		for(Map.Entry<String, Integer> entry:SortedMeme.entrySet())
-		{
-			Map<String,String> map=new LinkedHashMap<String,String>();
+    //write memes and scores to json
+	public void WriteMemes() {
+		JSONObject obj = new JSONObject();
+
+		JSONArray list = new JSONArray();
+
+		for (Map.Entry<String, Integer> entry : SortedMeme.entrySet()) {
+			Map<String, String> map = new LinkedHashMap<String, String>();
 			map.put("memeString", entry.getKey());
-			map.put("Score",entry.getValue().toString());
-			
+			map.put("Score", entry.getValue().toString());
+
 			list.put(map);
-			//list.put("{"+entry.getKey()+": "+entry.getValue().toString());
+			// list.put("{"+entry.getKey()+": "+entry.getValue().toString());
 		}
 		obj.put("memes", list);
-		String output=obj.toString();
+		String output = obj.toString();
 		try {
-			FileWriter writer=new FileWriter(new File("src\\main\\resources\\memes.json"));
+			FileWriter writer = new FileWriter(new File(
+					"src\\main\\resources\\memes.json"));
 			writer.write(output);
 			writer.flush();
 			writer.close();
 		} catch (IOException e) {
-			
-		}
-		
-	}
-	private HashMap<String,Integer> Meme;
-    private TreeMap<String,Integer> SortedMeme;
 
-public static void main(String[] args) throws JSONException
-{
-	Meme a=new Meme();
-	a.ReadMemes();
-	a.SortMemes();
-	a.ScoreMemes();
-	a.PrintMemes();
-	a.WriteMemes();
-}
+		}
+
+	}
+
+	private HashMap<String, Integer> Meme;
+	private TreeMap<String, Integer> SortedMeme;
+
+	public static void main(String[] args) throws JSONException {
+		Meme a = new Meme();
+		a.ReadMemes();
+		a.SortMemes();
+		a.ScoreMemes();
+		a.PrintMemes();
+		a.WriteMemes();
+	}
 }
